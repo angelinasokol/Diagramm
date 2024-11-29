a990c94fa0a3d237ffd258af054c46e38441bd0e
angelina <angelina@DESKTOP-CAQK9OI>
2024-11-30 00:35:26 +0700

    Добавьте файлы проекта.

---
+++ Diagramm/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace Diagramm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(14, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(222, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Введите количество продуктов:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(242, 20);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(348, 20);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(98, 22);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Подтвердить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(320, 367);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(300, 300);
+            this.chart1.TabIndex = 3;
+            this.chart1.Text = "chart1";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 46);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(907, 305);
+            this.dataGridView1.TabIndex = 4;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(738, 271);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(166, 67);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Построить \r\nдиаграмму";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(632, 367);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(287, 300);
+            this.textBox2.TabIndex = 6;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Column1";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Column2";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Column3";
+            this.Column3.Name = "Column3";
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(12, 650);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(104, 43);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Закрыть";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(931, 705);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Diagramm/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace Diagramm
+{
+    public partial class Form1 : Form
+    {
+        private List<Product> products = new List<Product>();
+        private int currentProductIndex = 1;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!int.TryParse(textBox1.Text, out int n) || n < 1 || n > 10)
+            {
+                MessageBox.Show("Введите корректное количество товаров (1-10).");
+                return;
+            }
+
+            dataGridView1.Rows.Clear(); // Очищаем таблицу
+            products.Clear(); // Очищаем список товаров
+            currentProductIndex = 1;
+
+            // Создаем верхние строки с информацией
+            dataGridView1.Rows.Add("Название товара", "Цена", "Количество", "Общая стоимость товара");
+
+            for (int i = 1; i < n; i++)
+            {
+                dataGridView1.Rows.Add();
+            }
+        }
+
+        public class Product
+        {
+            public string Name { get; set; }
+            public double Price { get; set; }
+            public int Quantity { get; set; }
+            public double TotalSales { get; set; }
+
+            public Product(string name, double price, int quantity, double totalSales)
+            {
+                Name = name;
+                Price = price;
+                Quantity = quantity;
+                TotalSales = totalSales;
+            }
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Series series = new Series("Диаграмма 1");
+            series.ChartType = SeriesChartType.Pie;
+
+            products.Clear();
+
+            double totalSales = 0; // Общая стоимость всех товаров
+
+            for (int rowIndex = 1; rowIndex < dataGridView1.Rows.Count; rowIndex++)
+            {
+                string productName = dataGridView1.Rows[rowIndex].Cells[0].Value as string;
+                if (productName == null || string.IsNullOrEmpty(productName))
+                {
+                    continue; // Пропустить пустые строки или строки с пустыми значениями
+                }
+
+                string priceStr = dataGridView1.Rows[rowIndex].Cells[1].Value as string;
+                string quantityStr = dataGridView1.Rows[rowIndex].Cells[2].Value as string;
+
+                if (priceStr == null || quantityStr == null)
+                {
+                    continue; // Пропустить строки, где цена или количество отсутствуют
+                }
+
+                double price = double.Parse(priceStr);
+                int quantity = int.Parse(quantityStr);
+
+                double currentTotalSales = price * quantity;
+
+                textBox2.Text += $"{productName}: Общая стоимость - {currentTotalSales.ToString("F2")} руб.\r\n";
+
+                products.Add(new Product(productName, price, quantity, currentTotalSales));
+            }
+        
+            foreach (Product product in products)
+            {
+                double percentage = (product.TotalSales / totalSales) * 100;
+
+                DataPoint dataPoint = new DataPoint();
+                dataPoint.AxisLabel = $"{product.Name} ({percentage.ToString("F2")}%)";
+                dataPoint.YValues = new double[] { product.TotalSales };
+                dataPoint.Label = "#PERCENT{F2}"; // Отображение процентов
+                series.Points.Add(dataPoint);
+            }
+
+            chart1.Series.Clear();
+            chart1.Series.Add(series);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
